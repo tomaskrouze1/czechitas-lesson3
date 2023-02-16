3cc2909101301e40f41051316f92891ab22eb569
DESKTOP-EDQ2DIM\Tomino <tomas.krouzel.jr@seznam.cz>
2023-02-16 18:32:18 +0100

    https://github.com/tomaskrouze1/js1-lekce5.git

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 // tady je místo pro náš program
+
+let jmeno = prompt("Napiš jméno:");
+let vek = Number(prompt("Napiš věk:"));
+
+document.querySelector(".vysledek").innerHTML=jmeno + ", věk:" + vek;
+
+
+
+
+
+
+
